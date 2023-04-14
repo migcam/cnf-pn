ad46776a7bf7a84d42e084cb7f04bc7eff4b89c0
Miguel Camarena <migcam@Miguels-MacBook-Air.local>
2023-04-14 13:49:53 -0400

    fix subtree tests

--- tests/SubTree_tests.cs
+++ tests/SubTree_tests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Xunit;
 using pkg;
 
@@ -11,36 +7,71 @@ namespace tests
     public class Copy_Tests
     {
 
-        private void GenericStringBuilderTest(string strinput , string stroutput , int i)
+        // SubTree copy tests
+        [Fact]
+        public void CopySubTree_Test1()  => SubTreeCopyGenericStringBuilderTest("IIqrIIpqIpr" , "Iqr",1);
+
+        [Fact]
+        public void CopySubTree_Test2() => SubTreeCopyGenericStringBuilderTest("IIqrIIpqIpr" , "IIpqIpr",4);
+        
+        [Fact]
+        public void CopySubTree_Test3() => SubTreeCopyGenericStringBuilderTest("IIqrIIpqIpr" , "q",2);
+
+        [Fact]
+        public void CopySubTree_Test4()  => SubTreeCopyGenericStringBuilderTest("IIqrIIpqIpr" , "Iqr",1);
+
+        [Fact]
+        public void CopySubTree_Test5() => SubTreeCopyGenericStringTest("IIqrIIpqIpr" , "IIpqIpr",4);
+
+        [Fact]
+        public void CopySubTree_Test6() => SubTreeCopyGenericStringTest("IIqrIIpqIpr" , "q",2);
+
+
+        // SubTree cut tests
+        [Fact]
+        public void CutSubTree_Test1()  => SubTreeCutGenericStringBuilderTest("IIqrIIpqIpr" , "Iqr",1, "IIIpqIpr");
+
+        [Fact]
+        public void CutSubTree_Test2() => SubTreeCutGenericStringBuilderTest("IIqrIIpqIpr" , "IIpqIpr",4, "IIqr");
+        
+        [Fact]
+        public void CutSubTree_Test3() => SubTreeCutGenericStringBuilderTest("IIqrIIpqIpr" , "q",2, "IIrIIpqIpr");
+
+        [Fact]
+        public void CutSubTree_Test4()  => SubTreeCutGenericStringTest("IIqrIIpqIpr" , "Iqr",1, "IIIpqIpr");
+
+        [Fact]
+        public void CutSubTree_Test5() => SubTreeCutGenericStringTest("IIqrIIpqIpr" , "q",2, "IIrIIpqIpr");
+
+        [Fact]
+        public void CutSubTree_Test6() => SubTreeCutGenericStringTest("IIqrIIpqIpr" , "q",2, "IIrIIpqIpr");
+
+
+        private void SubTreeCopyGenericStringBuilderTest(string strinput , string stroutput , int i)
         {
             StringBuilder input = new StringBuilder(strinput);
             StringBuilder output = SubTree.Copy(input, i);
             Assert.Equal(stroutput, output.ToString());
         }
-        private void GenericStringTest(string strinput , string stroutput , int i)
+        private void SubTreeCopyGenericStringTest(string strinput , string stroutput , int i)
         {
-            // string input = "IIqrIIpqIpr";
             string res = SubTree.Copy(strinput, i);
             Assert.Equal(stroutput, res);
         }
 
-        [Fact]
-        public void CopySubTree_Test1()  => GenericStringBuilderTest("IIqrIIpqIpr" , "Iqr",1);
+        private void SubTreeCutGenericStringBuilderTest(string strinput, string stroutput, int i, string cutedInput)
+        {
+            StringBuilder input = new StringBuilder(strinput);
+            StringBuilder output = SubTree.Cut(input, i);
+            Assert.Equal(stroutput, output.ToString());
+            Assert.Equal(cutedInput,input.ToString()); 
+        }
+        private void SubTreeCutGenericStringTest(string strinput, string stroutput , int i, string cutedInput)
+        {
+            string res = SubTree.Cut(ref strinput,i);
+            Assert.Equal(stroutput, res);
+            Assert.Equal(cutedInput,strinput.ToString()); 
+        }
 
-        [Fact]
-        public void CopySubTree_Test2() => GenericStringBuilderTest("IIqrIIpqIpr" , "IIpqIpr",4);
-        
-        [Fact]
-        public void CopySubTree_Test3() => GenericStringBuilderTest("IIqrIIpqIpr" , "q",2);
-
-        [Fact]
-        public void CopySubTree_Test4()  => GenericStringBuilderTest("IIqrIIpqIpr" , "Iqr",1);
-
-        [Fact]
-        public void CopySubTree_Test5() => GenericStringTest("IIqrIIpqIpr" , "IIpqIpr",4);
-
-        [Fact]
-        public void CopySubTree_Test6() => GenericStringTest("IIqrIIpqIpr" , "q",2);
-
-    }
+    }  
 }
