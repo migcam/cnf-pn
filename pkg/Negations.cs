c0435814f7583758cca13b3c8307915793e991f7
Miguel Camarena <migcam@Miguels-MacBook-Air.local>
2023-04-23 17:56:43 -0400

    optimize negations

--- pkg/Negations.cs
+++ pkg/Negations.cs
@@ -12,317 +12,90 @@ namespace pkg
         // !p and q === !p or !q
         public static void Delete_Negation(StringBuilder input)
         {
-            // deletes double negations (!!)
-            for (int i = 0; i < input.Length - 1;)
-            {
-                if (input[i].Equals('N') && input[i + 1].Equals('N'))
-                    input.Remove(i, 1);
-                else
-                    i++;
-            }
-            
             int pLength;
-            for (int i = 0; i < input.Length; i++)
+            for (int i = 0; i < input.Length-1; i++)
             {
-
-                if(input[i].Equals('C') && i>0 && input[i-1].Equals('N'))
+                if(input[i+1].Equals('C') && input[i].Equals('N'))
                 {
-                    input[i-1] = 'D';
-                    input[i]='N';
-                    pLength = SubTree.GetSubTreeLength(input,i+1);
-                    input.Insert(i+1+pLength,'N');
+                    input[i] = 'D';
+                    input[i+1]='N';
+                    pLength = SubTree.GetSubTreeLength(input,i+2);
+                    input.Insert(i+2+pLength,'N');                
                 }
-                else if(input[i].Equals('D') && i>0 && input[i-1].Equals('N'))
+                else if(input[i+1].Equals('D') && input[i].Equals('N'))
                 {
-                    input[i-1] = 'C';
-                    input[i]='N';
-                    pLength = SubTree.GetSubTreeLength(input,i+1);
-                    input.Insert(i+1+pLength,'N');
+                    input[i] = 'C';
+                    input[i+1]='N';
+                    pLength = SubTree.GetSubTreeLength(input,i+2);
+                    input.Insert(i+2+pLength,'N'); 
                 }
-                else if(input[i].Equals('N') && i>0 && input[i-1].Equals('N'))
+                
+                if(input[i].Equals('N') && input[i+1].Equals('N'))
                 {
-                    input.Remove(i-1,2);
+                    input.Remove(i--,2);
                 }
-
             }
         }
 
         public static string Delete_Negation(string input)
         {
-            // deletes double negations (!!)
-            for (int i = 0; i < input.Length - 1;)
+            for (int i = 0; i < input.Length-1; i++)
             {
-                if (input[i].Equals('N') && input[i + 1].Equals('N'))
-                    input  = input.Remove(i, 1);
-                else
-                    i++;
-            }
 
-            Stack<int> mystack = new Stack<int>();
-            
-            /*
-            0 = ambos procesados no cambios necesarios
-            1 = ambos procesado cambios necesarios
-            2 = hijo izquierdo procesado no cambios necesarios
-            3 = hijo izquierdo procesado cambios necesarios
-            */
-
-            mystack.Push(0);
-
-            for (int i = 1; i < input.Length ; i++)
-            {
-                //if (input[i].Equals('N'))
-                //    continue;
-
-                if (mystack.Peek() == 0 || mystack.Peek() == 2)
+                if(input[i+1].Equals('C') && input[i].Equals('N'))
                 {
-                    if(input[i - 1].Equals('N'))
-                    {
-                        if(input[i].Equals('D'))
-                        {
-                            // input[i] = 'C';
-                            input = SetChar(input,i,'C');
-                            input = input.Remove(--i, 1);
-                            mystack.Push(3);
-                        }
-                        else if(input[i].Equals('C'))
-                        {
-                            // input[i] = 'D';
-                            input  = SetChar(input,i,'D');
-                            input = input.Remove(--i, 1);
-                            mystack.Push(3);
-                        }
-                        else
-                        {
-                            if (mystack.Peek().Equals(2))
-                            {
-                                mystack.Pop();
-                                mystack.Push(0);
-                            }
-                            else
-                            {
-                                while (mystack.Count > 0 && (mystack.Peek().Equals(0) || mystack.Peek().Equals(1)))
-                                    mystack.Pop();
-                                
-                            }
-                        }
-                    }
-                    else
-                    {
-                        mystack.Push(2);
-                    }
-
+                    string p = SubTree.Copy(input,i+2);
+                    string q = SubTree.Copy(input,i+2+p.Length);
+                    input = string.Concat(input.Substring(0,i),"DN",p,"N",q,input.Substring(i+2+p.Length+q.Length));
                 }
-                else
+                else if(input[i+1].Equals('D') && input[i].Equals('N'))
                 {
-                    if (input[i - 1].Equals('N'))
-                    {
-                        input = input.Remove(--i, 1);
-
-                        if(input[i].Equals('C') || input[i].Equals('D'))
-                            mystack.Push(2);
-                        else
-                        {
-                            mystack.Pop();
-                            mystack.Push(1);
-                        }
-                    }
-                    else
-                    {
-                        if (input[i].Equals('N'))
-                            continue;
-                        
-                        if (input[i].Equals('C'))
-                        {
-                            // input[i] = 'D';
-                            input = SetChar(input,i,'D');
-                            mystack.Push(3);
-                        }
-                        else if (input[i].Equals('D'))
-                        {
-                            // input[i] = 'C';
-                            input = SetChar(input,i,'C');
-                            mystack.Push(3);
-                        }
-                        else
-                        {
-                            input = input.Insert(i++, "N");
-
-                            if (mystack.Peek().Equals(3))
-                            {
-                                mystack.Pop();
-                                mystack.Push(1);
-                            }
-                            else
-                            {
-                                while (mystack.Count > 0 && (mystack.Peek().Equals(0) || mystack.Peek().Equals(1))) 
-                                    mystack.Pop();
-                                
-                            }
-                        }
-                        
-                    }
+                    string p = SubTree.Copy(input,i+2);
+                    string q = SubTree.Copy(input,i+2+p.Length);
+                    input = string.Concat(input.Substring(0,i),"CN",p,"N",q,input.Substring(i+2+p.Length+q.Length));
                 }
+
+                if(input[i].Equals('N') && input[i+1].Equals('N'))
+                {
+                    input = input.Remove(i--,2);
+                }
+
             }
 
             return input;
         }
 
-        public static ReadOnlySpan<char> Delete_Negation(ReadOnlySpan<char> input)
+        public static ReadOnlySpan<char>  Delete_Negation(ReadOnlySpan<char> input)
         {
-            // deletes double negations (!!)
-            for (int i = 0; i < input.Length - 1;)
+            for (int i = 0; i < input.Length-1; i++)
             {
-                if (input[i].Equals('N') && input[i + 1].Equals('N'))
-                    input  = input.Remove(i, 1);
-                else
-                    i++;
-            }
 
-            Stack<int> mystack = new Stack<int>();
-            
-            /*
-            0 = ambos procesados no cambios necesarios
-            1 = ambos procesado cambios necesarios
-            2 = hijo izquierdo procesado no cambios necesarios
-            3 = hijo izquierdo procesado cambios necesarios
-            */
-
-            mystack.Push(0);
-
-            for (int i = 1; i < input.Length ; i++)
-            {
-                //if (input[i].Equals('N'))
-                //    continue;
-
-                if (mystack.Peek() == 0 || mystack.Peek() == 2)
+                if(input[i+1].Equals('C') && input[i].Equals('N'))
                 {
-                    if(input[i - 1].Equals('N'))
-                    {
-                        if(input[i].Equals('D'))
-                        {
-                            // input[i] = 'C';
-                            input = SetChar(input,i,'C');
-                            input = input.Remove(--i, 1);
-                            mystack.Push(3);
-                        }
-                        else if(input[i].Equals('C'))
-                        {
-                            // input[i] = 'D';
-                            input  = SetChar(input,i,'D');
-                            input = input.Remove(--i, 1);
-                            mystack.Push(3);
-                        }
-                        else
-                        {
-                            if (mystack.Peek().Equals(2))
-                            {
-                                mystack.Pop();
-                                mystack.Push(0);
-                            }
-                            else
-                            {
-                                while (mystack.Count > 0 && (mystack.Peek().Equals(0) || mystack.Peek().Equals(1)))
-                                    mystack.Pop();
-                                
-                            }
-                        }
-                    }
-                    else
-                    {
-                        mystack.Push(2);
-                    }
-
+                    var p = SubTree.Copy(input,i+2);
+                    var q = SubTree.Copy(input,i+2+p.Length);
+                    input = string.Concat(string.Concat(input.Slice(0,i),"DN",p,"N"),q,input.Slice(i+2+p.Length+q.Length));
                 }
-                else
+                else if(input[i+1].Equals('D') && input[i].Equals('N'))
                 {
-                    if (input[i - 1].Equals('N'))
-                    {
-                        input = input.Remove(--i, 1);
-
-                        if(input[i].Equals('C') || input[i].Equals('D'))
-                            mystack.Push(2);
-                        else
-                        {
-                            mystack.Pop();
-                            mystack.Push(1);
-                        }
-                    }
-                    else
-                    {
-                        if (input[i].Equals('N'))
-                            continue;
-                        
-                        if (input[i].Equals('C'))
-                        {
-                            // input[i] = 'D';
-                            input = SetChar(input,i,'D');
-                            mystack.Push(3);
-                        }
-                        else if (input[i].Equals('D'))
-                        {
-                            // input[i] = 'C';
-                            input = SetChar(input,i,'C');
-                            mystack.Push(3);
-                        }
-                        else
-                        {
-                            input = input.Insert(i++, "N");
-
-                            if (mystack.Peek().Equals(3))
-                            {
-                                mystack.Pop();
-                                mystack.Push(1);
-                            }
-                            else
-                            {
-                                while (mystack.Count > 0 && (mystack.Peek().Equals(0) || mystack.Peek().Equals(1))) 
-                                    mystack.Pop();
-                                
-                            }
-                        }
-                        
-                    }
+                    var p = SubTree.Copy(input,i+2);
+                    var q = SubTree.Copy(input,i+2+p.Length);
+                    input = string.Concat(string.Concat(input.Slice(0,i),"CN",p,"N"),q,input.Slice(i+2+p.Length+q.Length));
                 }
+
+                if(input[i].Equals('N') && input[i+1].Equals('N'))
+                {
+                    input = input.Remove(i--,2);
+                }
+
             }
 
             return input;
         }
-
-        private static string SetChar(string input, int i, char character)
-        {
-            string beforei = "";
-            string afteri = "";
-
-            if (i > 0)
-                beforei = input.Substring(0, i);
-            if (i < input.Length - 2)
-                afteri = input.Substring(i + 1);
-
-            return beforei + character + afteri;
-        }
-
-        private static ReadOnlySpan<char> SetChar(ReadOnlySpan<char> input, int i, char character)
-        {
-            ReadOnlySpan<char> beforei = "";
-            ReadOnlySpan<char> afteri = "";
-            ReadOnlySpan<char> charSpan = character.ToString();
-
-            if (i > 0)
-                beforei = input.Slice(0, i);
-            if (i < input.Length - 2)
-                afteri = input.Slice(i + 1);
-
-            // input = beforei + character + afteri;
-            return string.Concat(beforei,charSpan,afteri);
-        }
-
-        
-
 
     }
 
-    public static class Extension
+    public static class ReadOnlySpanExtensions
     {
         public static ReadOnlySpan<char> Remove(this ReadOnlySpan<char> input, int idx, int length)
         {
