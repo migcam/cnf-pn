b0dbbd47b4d66099903b99e457ce5ec2304ce608
Miguel Camarena <migcam@Miguels-MacBook-Air.local>
2023-04-23 13:37:39 -0400

    edit negations benchmarks

--- benchmarks/NegationBenchmarks.cs
+++ benchmarks/NegationBenchmarks.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using pkg;
 using BenchmarkDotNet.Attributes;
+using System.Linq.Expressions;
 
 namespace benchmarks
 {
@@ -14,6 +15,26 @@ namespace benchmarks
             Negations.Delete_Negation(input);
         }
 
+        private void GenericString(string input)
+        {
+            Negations.Delete_Negation(input);
+        }
+
+        private void GenericSpan(ReadOnlySpan<char> input)
+        {
+            Negations.Delete_Negation(input);
+        }
+
+        private void GenericExpression1(CnfExpressionParser input)
+        {
+            input.MoveNegationsInward();
+        }
+
+        private void GenericExpression2(CnfExpressionParser input, MoveNegationsInwardVisitor visitor)
+        {
+            visitor.Visit(input._expression);
+        }
+
         [Benchmark]
         public void NegationStringBuilder1() => GenericStringBuilder(new StringBuilder("DNDNqrDNDNpqDNpr"));
         [Benchmark]
@@ -23,35 +44,43 @@ namespace benchmarks
         [Benchmark]
         public void NegationStringBuilder4() => GenericStringBuilder(new StringBuilder("DNDNpqDpNp"));
 
-        // [Benchmark]
-        // public void test5()
-        // {
-        //     string input = "DNDNqrDNDNpqDNpr";
-        //     string output = Negations.Delete_Negation(input);
-        //     Assert.Equal("DCqNrDCpNqDNpr", output);
-        // }
-
-        // [Benchmark]
-        // public void test6()
-        // {
-        //     string input = "NCNpp";
-        //     string output = Negations.Delete_Negation(input);
-        //     Assert.Equal("DpNp", output);
-        // }
+        [Benchmark]
+        public void NegationString1() => GenericString("DNDNqrDNDNpqDNpr");
+        [Benchmark]
+        public void NegationString2() => GenericString("NCNpp");
+        [Benchmark]
+        public void NegationString3() => GenericString("NNNNNNNp");
+        [Benchmark]
+        public void NegationString4() => GenericString("DNDNpqDpNp");
 
         [Benchmark]
-        public void Negation_3_String()
-        {
-            string output = Negations.Delete_Negation("NNNNNNNp");
-        }
+        public void NegationSpan1() => GenericSpan("DNDNqrDNDNpqDNpr");
+        [Benchmark]
+        public void NegationSpan2() => GenericSpan("NCNpp");
+        [Benchmark]
+        public void NegationSpna3() => GenericSpan("NNNNNNNp");
+        [Benchmark]
+        public void NegationSpan4() => GenericSpan("DNDNpqDpNp");
 
-        // [Benchmark]
-        // public void test8()
-        // {
-        //     string input =  "DNDNpqDpNp";
-        //     string output = Negations.Delete_Negation(input);
-        //     Assert.Equal("DCpNqDpNp", output);
-        // }
+
+        [Benchmark]
+        public void NegationExpression1() => GenericExpression1(new CnfExpressionParser("DNDNqrDNDNpqDNpr"));
+        [Benchmark]
+        public void NegationExpression2() => GenericExpression1(new CnfExpressionParser("NCNpp"));
+        [Benchmark]
+        public void NegationExpression3() => GenericExpression1(new CnfExpressionParser("NNNNNNNp"));
+        [Benchmark]
+        public void NegationExpression4() => GenericExpression1(new CnfExpressionParser("DNDNpqDpNp"));
+
+        [Benchmark]
+        public void NegationExpression5() => GenericExpression2(new CnfExpressionParser("DNDNqrDNDNpqDNpr"), new MoveNegationsInwardVisitor());
+        [Benchmark]
+        public void NegationExpression6() => GenericExpression2(new CnfExpressionParser("NCNpp"), new MoveNegationsInwardVisitor());
+        [Benchmark]
+        public void NegationExpression7() => GenericExpression2(new CnfExpressionParser("NNNNNNNp"), new MoveNegationsInwardVisitor());
+        [Benchmark]
+        public void NegationExpression8() => GenericExpression2(new CnfExpressionParser("DNDNpqDpNp"), new MoveNegationsInwardVisitor());
 
     }
+
 }
